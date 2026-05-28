3e3fcff08522b2ffc118500f403c948c09dc3829
matthew blakney <matthewblakney@matthews-MacBook-Pro-2.local>
2026-05-28 13:09:53 -0400

    POC-2919 - Adding feature to allow theme names to be specified to have the extension applied to

--- Plugin/LayoutObserverPlugin.php
+++ Plugin/LayoutObserverPlugin.php
@@ -1,28 +1,54 @@
 <?php
 namespace Hyva\PunchoutGateway\Plugin;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\DesignInterface;
 use Punchout\Gateway\Helper\GatewayEnv as EnvHelper;
+use Punchout\Gateway\Helper\Utils as UtilsHelper;
 
 class LayoutObserverPlugin
 {
+    protected const CONFIG_PATH_DETECT_HYVA = 'hyva_punchoutgateway/general/detect_hyva';
+
+    protected const CONFIG_PATH_ADDITIONAL_THEMES = 'hyva_punchoutgateway/general/additional_themes';
+    protected $scopeConfig;
     protected $design;
     protected $envHelper;
+    protected $utilsHelper;
 
     public function __construct(
+        ScopeConfigInterface $scopeConfig,
         DesignInterface $design,
-        EnvHelper $envHelper
+        EnvHelper $envHelper,
+        UtilsHelper $utilsHelper
     ) {
+        $this->scopeConfig = $scopeConfig;
         $this->design = $design;
         $this->envHelper = $envHelper;
+        $this->utilsHelper = $utilsHelper;
     }
 
     public function afterExecute($subject, $result, $observer)
     {
-        $theme = $this->design->getDesignTheme();
+        $detectHyva = $this->scopeConfig->getValue(
+            static::CONFIG_PATH_DETECT_HYVA, 
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        $additionalThemes = $this->scopeConfig->getValue(
+            static::CONFIG_PATH_ADDITIONAL_THEMES, 
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+        $additionalThemes = $this->utilsHelper->parseTextareaValues($additionalThemes);
+
+        $theme = $this->design->getDesignTheme()->getThemePath();
         
-        if (stripos($theme->getCode(), 'hyva') === false) {
-            return $result;
+        if (!in_array($theme, $additionalThemes)) {
+            if (
+                !$detectHyva || ($detectHyva && stripos($theme, 'hyva') === false)
+            ) {
+                return $result;
+            }
         }
 
         $action = $observer->getEvent()->getFullActionName();
